413f2eca3aac414b5a4133e35d5ee2ecd192fb39
Richasy <Thansy@foxmail.com>
2025-05-11 22:02:59 +0800

    更新引用

--- src/Samples/WinUISample/GlobalDependencies.cs
+++ src/Samples/WinUISample/GlobalDependencies.cs
@@ -4,7 +4,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Dispatching;
 using Richasy.BiliKernel;
-using Richasy.ReaderKernel;
+using Richasy.WebDavKernel;
 using Richasy.WinUIKernel.Share;
 using Richasy.WinUIKernel.Share.Toolkits;
 using RichasyKernel;
